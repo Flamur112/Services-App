9bad86389b774cd64b8bdc7316fef6f3e9fae7ec
Flamur <34803365+Flamur112@users.noreply.github.com>
2024-03-16 04:08:59 -1100

    small changes

---
+++ Services-App/resources/views/frontend/home.blade.php
@@ -0,0 +1,19 @@
+
+
+@extends('frontend.master')
+
+@section('footer')
+
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2024 Composed Apparels. All Rights Reserved.
+            Skopje, North Macedonia
+
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  @endsection
--- Services-App/routes/web.php
+++ Services-App/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TemplateController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\Template\Template;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [TemplateController::class, 'index']);
